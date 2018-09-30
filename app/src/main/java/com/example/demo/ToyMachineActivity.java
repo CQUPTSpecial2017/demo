342b024361be618e6063d6f482d5c4f18c114b44
nick <948601678@qq.com>
2018-09-30 22:20:47 +0800

    添加win界面 facebook接口接入

--- app/src/main/java/com/example/demo/ToyMachineActivity.java
+++ app/src/main/java/com/example/demo/ToyMachineActivity.java
@@ -2,12 +2,27 @@ package com.example.demo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
 
 public class ToyMachineActivity extends AppCompatActivity {
 
+ ImageView continueb;
+ ImageView backer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_toy_machine);
+        init();
+    }
+    public void  init(){
+
+       backer=(ImageView)findViewById(R.id.backer);
+       Glide.with(this).load(R.drawable.backer).into(backer);
+        continueb=(ImageView)findViewById(R.id.continueBlue);
+        Glide.with(this).load(R.drawable.continuebl).into(continueb);
+     continueb.bringToFront();
+
     }
 }
